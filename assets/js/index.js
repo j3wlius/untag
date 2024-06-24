46bb10a9a56cbffa8f16703f2fd376f5ca1f3a2a
jewlius <tamalejulius5@gmail.com>
2024-06-24 23:59:57 +0300

    untag

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,4 +49,21 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 const accordionBtn = document.querySelectorAll(".accordion-menu");
 const accordionMenu = document.querySelectorAll(".submenu-category-list");
 
+for (let i = 0; i < accordionBtn.length; i++) {
+	accordionBtn[i].addEventListener("click", function () {
+		const clickedBtn =
+			this.nextElementSibling.classList.contains("active");
 
+		for (let i = 0; i < accordion.length; i++) {
+			if (clickedBtn) break;
+
+			if (accordion[i].classList.contains("active")) {
+				accordion[i].classList.remove("active");
+				accordionBtn[i].classList.remove("active");
+			}
+		}
+
+		this.nextElementSibling.classList.toggle("active");
+		this.classList.toggle("active");
+	});
+}
